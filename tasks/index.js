9c7b5289116b37083cf675985debe2cda0871912
Nicolas Mahe <nicolas@mahe.me>
2019-06-04 17:11:32 +0700

    update task handler to single output

--- tasks/index.js
+++ tasks/index.js
@@ -1,13 +1,7 @@
 module.exports = ({
   web3,
   erc20ABI
-}) => (method) => async (inputs, { success, error }) => {
-  try {
-    const contract = new web3.eth.Contract(erc20ABI, inputs.contractAddress)
-    const data = await method(contract, inputs)
-    return success(data)
-  } catch (err) {
-    console.error(err)
-    return error({message: err.message})
-  }
+}) => (method) => (inputs) => {
+  const contract = new web3.eth.Contract(erc20ABI, inputs.contractAddress)
+  return method(contract, inputs)
 }
